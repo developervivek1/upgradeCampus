e2f81096382243e6a8e0b9ee48a227df4ec73517
developervivek1 <vivekpathak.emmy1234@gmail.com>
2022-06-21 18:00:31 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
+
+// on scroll navbar
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-80px";
   }
   prevScrollpos = currentScrollPos;
 }
 
 
-
+// mega menu
  document.addEventListener("DOMContentLoaded", function(){
                 /////// Prevent closing from click inside dropdown
                 document.querySelectorAll('.dropdown-menu').forEach(function(element){
