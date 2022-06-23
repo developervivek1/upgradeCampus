4b43511be8839f00646f78dde288dab396ab4df2
developervivek1 <vivekpathak.emmy1234@gmail.com>
2022-06-23 16:19:10 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const hamberger = document.querySelector('header .navbar-toggler');
+const navbarMenu = document.querySelector('header .navbar-collapse');
+const overlay = document.querySelector('header .overlay');
+const body = document.querySelector('body');
+const limenu = document.querySelectorAll('header .navbar-nav li');
 
 // on scroll navbar
 var prevScrollpos = window.pageYOffset;
@@ -14,11 +19,45 @@ var currentScrollPos = window.pageYOffset;
 
 // mega menu
  document.addEventListener("DOMContentLoaded", function(){
-                /////// Prevent closing from click inside dropdown
-                document.querySelectorAll('.dropdown-menu').forEach(function(element){
-                    element.addEventListener('click', function (e) {
-                        e.stopPropagation();
-                    });
-                })
-            }); 
-            // DOMContentLoaded  end
+      /////// Prevent closing from click inside dropdown
+      document.querySelectorAll('.dropdown-menu').forEach(function(element){
+          element.addEventListener('click', function (e) {
+              e.stopPropagation();
+          });
+      })
+  }); 
+  // DOMContentLoaded  end
+
+
+// hamberger menu
+hamberger.addEventListener('click',()=>
+{
+    if(!navbarMenu.classList.contains('show'))
+    {
+      overlay.classList.add('active');
+      body.style.overflow="hidden";
+    }
+})
+
+overlay.addEventListener('click',()=>
+{
+  if(navbarMenu.classList.contains('show'))
+  {
+    navbarMenu.classList.remove('show');
+    overlay.classList.remove('active');
+    body.style.overflow="auto";
+  }
+})
+
+// limenu.forEach(elem => {
+//   elem.addEventListener('click',(e)=>
+//   {
+//     let navlink = e.target.closest('li').querySelector('.nav-link');
+//     let active_navlink = e.target.closest('ul').querySelector('.nav-link.show');
+//     if(!navlink.classList.contains('show'))
+//     {
+//       active_navlink.classList.remove('show');
+//       navlink.classList.add('show');
+//     }
+//   })
+// });
