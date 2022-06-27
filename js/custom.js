9089cb9832001750c30dde15cd68487fd3ae105f
developervivek1 <vivekpathak.emmy1234@gmail.com>
2022-06-27 10:18:08 +0530

    updtaed

--- js/custom.js
+++ js/custom.js
@@ -56,6 +56,22 @@ $('.sel__box__options').click(function() {
 });
 
 
+/* ===== Logic for Form Validation ===== */
 
-
+         (function() {
+           window.addEventListener('load', function() {
+             var forms = document.getElementsByClassName('needs-validation');
+       
+             var validation = Array.prototype.filter.call(forms, function(form) {
+               form.addEventListener('submit', function(event) {
+                 if (form.checkValidity() === false) {
+                   event.preventDefault();
+                   event.stopPropagation();
+                 }
+                 form.classList.add('was-validated');
+               }, false);
+             });
+           }, false);
+         })();
+ 
 
